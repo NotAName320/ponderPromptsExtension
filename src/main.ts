b6815246efc0c8da5771a9914f1c31a8e1342498
Janelle Yan <janelle.yan@mail.utoronto.ca>
2024-10-27 20:38:02 -0400

    Added documentation for main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import $ from 'jquery'
 import KeyUpEvent = JQuery.KeyUpEvent;
 
+/**
+ * Main class responsible for handling user input in the timer extension.
+ * This class initializes event listeners for the html input fields to manage the focus
+ * and establishes minimum and maximum values.
+ */
 class Main {
+    // Initialize the class by setting up event listeners.
     constructor() {
         this.init();
     }
 
+    /**
+     * Sets up event listeners for the html input fields.
+     * These listeners are added for keyup events on the minutes and seconds input fields
+     * and on each element with the class minMax.
+     */
     init() {
         $(() => {
             $("#minutes")[0].addEventListener("keyup", this.moveCursor);
@@ -18,12 +29,21 @@ class Main {
         });
     }
 
+    /**
+     * Moves the cursor focus to the seconds input when the HTMLInputElement reaches 3 or more characters.
+     * @param this - the HTMLInputElement where the event occurred.
+     */
     moveCursor(this: HTMLInputElement): any {
         if(this.value.length >= 3) {
             $("#seconds").trigger("focus");
         }
     }
 
+    /**
+     * Moves the cursor focus back to the minutes input if the user presses backspace and the HTMLInputElement empty.
+     * @param this - The HTMLInputElement where the event occurred.
+     * @param ev - The keyboard event that triggered this function.
+     */
     moveCursorBack(this: HTMLElement, ev: KeyboardEvent): any {
         if (ev.code !== "Backspace") return;
         if((<HTMLInputElement>this).value.length === 0) {
@@ -31,6 +51,10 @@ class Main {
         }
     }
 
+    /**
+     * Establishes min and max values, trims the decimal, and ensures the value is an integer for the input value.
+     * @param this - The HTMLInputElement where the event occurred.
+     */
     enforceMinMax(this: HTMLInputElement) {
         if (this.value != "" && this.value != ".") {
             //chop off decimal
@@ -46,4 +70,5 @@ class Main {
 
 }
 
+// Instantiates the Main class to activate the event listeners
 new Main();
