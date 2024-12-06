b5107f85b3af5ecfc908189a283433786caeed52
Ray Chen <rc144@outlook.com>
2024-12-06 01:01:15 -0500

    add question types enum

--- src/questions.ts
+++ src/questions.ts
@@ -1,12 +1,18 @@
+export enum QuestionTypes {
+    TEXT_BOX, // only this one works for now
+    MULTIPLE_CHOICE,
+    MULTIPLE_SELECT
+}
+
 export const questions = [
     {
         questionId: "sample",
         questionText: "This is a sample question",
-        questionType: "textbox"
+        questionType: QuestionTypes.TEXT_BOX
     },
     {
         questionId: "sample2",
         questionText: "This is a second sample question",
-        questionType: "textbox"
+        questionType: QuestionTypes.TEXT_BOX
     }
 ]
